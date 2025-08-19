0cddc6000888e4a718248904e8576a8a71813130
mgcodeur <mgcodeur@gmail.com>
2025-08-19 15:34:58 +0300

    release: v1

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Mgcodeur\LaravelTranslationLoader\Traits\FlushesCache;
 
+/**
+ * @property int $id
+ * @property int $language_id
+ * @property string $key
+ * @property string|null $value
+ * @property-read Language|null  $language
+ */
 class Translation extends Model
 {
     use FlushesCache;
